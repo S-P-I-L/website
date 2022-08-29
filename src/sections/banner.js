d6f6a47de6700fc2aebe6871c9f1386dcf15eac6
giwajossy <giwajosef@gmail.com>
2022-08-29 07:07:01 +0100

    update banner

--- src/sections/banner.js
+++ src/sections/banner.js
@@ -6,8 +6,8 @@ import ModalVideo from 'react-modal-video';
 import { Link } from 'components/link';
 // import { FaPlayCircle } from 'react-icons/fa';
 import BannerBG from 'assets/bannerBg.png';
-import BannerThumb from 'assets/banner-4.png';
-// import BannerThumb from 'assets/banner-thumb.png';
+// import BannerThumb from 'assets/banner-4.png';
+import BannerThumb from 'assets/banner-thumb.png';
 
 import client1 from 'assets/sponsor/paypal.svg';
 import client2 from 'assets/sponsor/google.svg';
@@ -49,7 +49,6 @@ export default function Banner() {
           </Heading>
           <Text as="p" variant="heroSecondary">
             We provide a comprehensive suite of solutions to address the digital needs of organizations, limited partners, and entrepreneurs across various industries.
-            {/* We empower exceptional teams and companies across various industries including medicine, law, agriculture, and fashion. */}
           </Text>
           <Flex>
 
@@ -120,10 +119,10 @@ const styles = {
     imageBox: {
       display: ['none', null, null, 'block'],
       justifyContent: 'center',
-      // ml: [0, null, null, '-110px', '-115px', '-150px', '-210px', '-270px'],
-      // mr: [0, null, null, '-145px', '-160px', '-180px', '-220px', '-290px'],
-      // mt: [0, null, null, '40px', 4, 7, 0],
-      // mb: [0, null, null, null, '-45px', '-70px', null, '-115px'],
+      ml: [0, null, null, '-110px', '-115px', '-150px', '-210px', '-270px'],
+      mr: [0, null, null, '-145px', '-160px', '-180px', '-220px', '-290px'],
+      mt: [0, null, null, '40px', 4, 7, 0],
+      mb: [0, null, null, null, '-45px', '-70px', null, '-115px'],
       overflow: 'hidden',
       textAlign: 'right',
       width: '100%',
